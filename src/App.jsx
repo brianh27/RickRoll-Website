476820cf4daf0698817d033cfb418f74cf607316
brianh27 <brianh27@lakesideschool.org>
2025-03-13 21:32:23 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useEffect } from 'react';
+import {useState, useEffect } from 'react';
+import PocketBase from 'pocketbase';
 
 
+async function upload(name,setSubmit,setDisabed){
+  setDisabed(true)
+  const pb = new PocketBase('https://ai-study-guides.pockethost.io');
+  // example create data
+  const data = {
+    "Name": name
+  };
+
+  const record = await pb.collection('LakesideLeaks').create(data);
+  setSubmit(true)
+  
+  
+}
 
 function App() {
   
 
-  
+  const [submit,setSubmit]=useState(false)
+  const [name,setName]=useState('')
+  const [disabed,setDisabed]=useState(false)
   useEffect(() => {
-    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
-  }, []); 
-
+    if (submit){
+      window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+    }
+  }, [submit]); 
+  
   return (
     <div className="App">
-      <h1>Redirecting (I'm sorry)...</h1>
+      {submit?<h1>Redirecting (I'm sorry)...</h1>:
+      <div>
+        <h1>Welcome to Lakeside Leaks</h1>
+        <p>In order to mantain the anonymity of our contributors, we do not collect any information</p>
+        <p>Please enter your name to get started.</p>
+        <input onChange={(e)=>setName(e.target.value)} value={name}></input>
+        <button disabled={disabed} onClick={()=>upload(name,setSubmit,setDisabed)}>Submit</button>
+      </div>
+      }
     </div>
   );
 }
